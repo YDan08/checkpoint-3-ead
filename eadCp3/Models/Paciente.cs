e3484ce84ba70ae12a98650b3bce335c05f1ec44
YDan08 <dasyamashita@gmail.com>
2022-10-24 17:38:15 -0300

    feat: adicionar mais atributos

--- eadCp3/Models/Paciente.cs
+++ eadCp3/Models/Paciente.cs
@@ -4,6 +4,12 @@
     {
         public int Id { get; set; }
 
-        public string Nome { get; set; }
+        public string? Nome { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
+
+        public string? Cpf { get; set; }
     }
 }
